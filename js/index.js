cd8377cffb86635d18a2ec54f13482e185b1dac0
Audra Tallman <audi.tallman@gmail.com>
2019-06-25 16:55:39 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,3 +4,27 @@ function greet(customer) {
     console.log(`Hi ${customer}, my name is ${this.name}!`);
 }
  
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        console.log("'this' is defined: ", this);
+        return event.keywords.some(function(word) {
+            console.log("'this' is now undefined: ", this);
+            return this.interests.includes(word);
+        });
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
